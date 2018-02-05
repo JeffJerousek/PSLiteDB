2b04cf5bf80593bed0fb86f4731b72b8281ff980
Kiran Reddy <tanyasweet1933@gmail.com>
2018-02-05 02:30:47 +0100

    added xy functions

--- PSLiteDB.psm1
+++ PSLiteDB.psm1
@@ -1,5 +1,7 @@
-$script:QueryLDB = [LiteDB.Query]
+using namespace PSLiteDB
+using namespace LiteDB
 
+$script:QueryLDB = [LiteDB.Query]
 
 function ConvertTo-LiteDbBSON
 {
@@ -65,6 +67,182 @@ function ConvertTo-LiteDbBSON
     }
 }
 
+
+function Update-XYRecord
+{
+    [CmdletBinding()]
+    [ALias("Uxy")]
+    param 
+    (
+        [Parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+        [String]
+        $Collection = "XY"
+    )
+    
+    begin
+    {
+        
+    }
+    
+    process
+    {
+        $tagsource = Join-Path $env:APPDATA -ChildPath XYPlorer\Tag.dat
+        [CRUD]::Upsert([CRUD]::GetXYplorerFile($tagsource),$Collection)       
+    }
+    
+    end
+    {
+
+    }
+}
+
+function Set-XYIndex
+{
+    [CmdletBinding()]
+    [ALias("CreateXYIndex")]
+    param 
+    (
+        [Parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+        [String]
+        $Collection = "XY"
+    )
+    
+    begin
+    {
+        
+    }
+    
+    process
+    {
+        [CRUD]::CreateIndex($Collection)       
+    }
+    
+    end
+    {
+
+    }
+}
+
+function Get-XYIndex
+{
+    [CmdletBinding()]
+    [ALias("gixy")]
+    param 
+    (
+        [Parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+        [String]
+        $Collection = "XY"
+    )
+    
+    begin
+    {
+        
+    }
+    
+    process
+    {
+        [CRUD]::GetIndex($Collection)       
+    }
+    
+    end
+    {
+
+    }
+}
+
+
+function Find-XYRecord
+{
+    [CmdletBinding()]
+    [ALias("FXY")]
+    param 
+    (
+        [Parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+        [String]
+        $Collection = "XY",
+
+        [Alias("Tags")]
+        [Parameter(ValueFromPipelineByPropertyName,Position = 0)]
+        [String[]]
+        $Tag,    
+        
+        #[Alias("BaseName")]
+        [Parameter(ValueFromPipelineByPropertyName,Position = 1)]
+        [String[]]
+        $BaseName ,
+        
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [int]
+        $Limit = 50,
+
+        [Parameter(ValueFromPipelineByPropertyName)]
+        [int]
+        $Skip = 0          
+
+    )
+    
+    begin
+    {
+        
+    }
+    
+    process
+    {           
+        if ($BaseName -and $Tag) 
+        {
+
+            Write-Verbose "name::$basename & Tag::$Tag"
+            [CRUD]::FindbyTagNameList($BaseName,$Tag,$Collection)               
+        }
+        elseif ($BaseName -and (-not $Tag)) 
+        {
+            Write-Verbose "name::$basename"
+            [CRUD]::FindbyNameList($BaseName,$Collection)
+        }
+        elseif ($Tag -and (-not $BaseName)) 
+        {
+            Write-Verbose "Tag::$Tag"
+            [CRUD]::FindbyTagList($Tag,$Collection)
+        }
+        else 
+        {
+            [CRUD]::List($Limit,$Skip,$Collection)
+        }
+             
+    }
+    
+    end
+    {
+
+    }
+}
+
+
+function Remove-XYOrphan
+{
+    [CmdletBinding()]
+    param 
+    (
+        [Parameter(ValueFromPipelineByPropertyName,ValueFromPipeline)]
+        [String]
+        $Collection = "XY"
+    )
+    
+    begin
+    {
+        
+    }
+    
+    process
+    {
+        [CRUD]::RemoveOrphans($Collection)       
+    }
+    
+    end
+    {
+
+    }
+}
 
 
 Export-ModuleMember -Variable QueryLDB -Function ConvertTo-LiteDBBSON
